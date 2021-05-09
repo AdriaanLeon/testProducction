445c9848ff981701bf05ceac7e02a737db71a7d8
AdriaanLeon <leon.adriand@gmail.com>
2021-05-08 22:08:36 -0500

    I added the class bank

---
+++ partOne/Models/Bank.cs
@@ -0,0 +1,10 @@
+namespace partOne.Models
+{
+    public class Bank
+    {
+        public int Id { get; set; }
+        public string NameBank { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
